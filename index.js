2b619512934fa78f74c450eb9839424627914a05
faizal-siddiqui <faizalsidp@gmail.com>
2023-01-14 22:50:25 +0530

    deploy server

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; // any port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
